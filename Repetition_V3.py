68c686b94c5429edb92a6e20766e3472e515c4a0
theminer3746 <theminer@theminerdev.com>
2017-08-25 16:21:07 +0700

    [WIP] Create Repetition_V3

---
+++ Repetition_V3.py
@@ -0,0 +1,38 @@
+n = int(input())
+
+x_list = list()
+y_list = list()
+
+# Retrieve all possible x, y combination
+for x in range(n):
+    for y in range(n):
+        if x**2 + y**2 == n:
+            x_list.append(x)
+            y_list.append(y)
+
+# Remove duplication
+x_to_remove = list()
+y_to_remove = list()
+
+for i in range(len(x_list)):
+    if y_list.count(x_list[i]) != 0:
+        index_to_remove = y_list.index(x_list[i])
+        if index_to_remove > i:  # If index_to_remove < i, that mean we already passed it
+            x_to_remove.append(x_list[index_to_remove])
+            y_to_remove.append(y_list[index_to_remove])
+
+for i in range(len(x_to_remove)):
+    x_list.remove(x_to_remove[i])
+    y_list.remove(y_to_remove[i])
+
+# Sorting answer
+for i in range(len(x_list)):
+    if x_list[i] > y_list[i]:
+        x_list[i], y_list[i] = y_list[i], x_list[i]
+
+# Print the answer(s)
+if len(x_list) == 0:
+    print('No solution')
+else:
+    for i in range(len(x_list)):
+        print(x_list[i], y_list[i])
