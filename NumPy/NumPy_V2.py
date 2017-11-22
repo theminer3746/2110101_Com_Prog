abf1e93cfd27c85c2ca8d9d13f7c0194e2e08bec
theminer3746 <theminer@theminerdev.com>
2017-11-22 14:13:48 +0700

    WIP Numpy_V2

---
+++ NumPy/NumPy_V2.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+cost = np.array([int(x) for x in input().strip().split()])
+
+rental = np.array([[int(x) for x in input().strip().split()], [int(x) for x in input().strip().split()], [int(x) for x in input().strip().split()], [int(x) for x in input().strip().split()]])
+total_rental = 0
+sum_by_day = np.sum(rental, axis=0)
+
+print(['Mon', 'Tue', 'Wed', 'Thu', 'Fri'][np.argmax(sum_by_day)], float(np.max(sum_by_day)))
+print(*[float(x) for x in cost.dot(rental)])
