6f2a411c07272dc6ee692ba6df263f73433ff5d9
theminer3746 <theminer@theminerdev.com>
2017-11-28 21:11:09 +0700

    Code formatting

--- NumPy/NumPy_V2.py
+++ NumPy/NumPy_V2.py
@@ -2,7 +2,12 @@ import numpy as np
 
 cost = np.array([int(x) for x in input().strip().split()])
 
-rental = np.array([[int(x) for x in input().strip().split()], [int(x) for x in input().strip().split()], [int(x) for x in input().strip().split()], [int(x) for x in input().strip().split()]])
+rental = np.array([
+    [int(x) for x in input().strip().split()],
+    [int(x) for x in input().strip().split()],
+    [int(x) for x in input().strip().split()],
+    [int(x) for x in input().strip().split()]
+])
 total_rental = 0
 sum_by_day = np.sum(rental, axis=0)
 
