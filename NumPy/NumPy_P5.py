68c96dc0564ac23dcc2162f50ea20793d5af7f33
theminer3746 <theminer@theminerdev.com>
2017-12-07 23:29:16 +0700

    Finished NumPy_P5

--- NumPy/NumPy_P5.py
+++ NumPy/NumPy_P5.py
@@ -1,9 +1,8 @@
 import numpy as np
-import math
 
 
 def fib(n, k):
-    output = power(np.array([[0, 1], [1, 1]]), n)
+    output = power(np.array([[0, 1], [1, 1]]), n, k)
 
     return output[0][1]
 
@@ -11,19 +10,17 @@ def fib(n, k):
 result = dict()
 
 
-def power(matrix, n):
-    print('N :', n)
+def power(matrix, n, k):
     if n not in result:
         if n == 0:
             result[n] = np.identity(2, dtype=int)
         elif n % 2 == 0:
-            result[n] = power(power(matrix, math.floor(n / 2)), 2)
-        else:
-            result[n] = matrix.dot(power(power(matrix, math.floor(n / 2)), 2))
+            result[n] = power(matrix, n // 2, k).dot(power(matrix, n // 2, k)) % k
+        elif n % 2 == 1:
+            result[n] = matrix.dot(power(matrix, n // 2, k).dot(power(matrix, n // 2, k)) % k) % k
 
     return result[n]
 
 
 n, k = [int(e) for e in input().split()]
 print(fib(n, k))
-
